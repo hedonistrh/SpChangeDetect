963f3cad4d489d38406f6e0cc23e655d2b0c7558
hedonistrh <herdoganturkey@gmail.com>
2018-09-07 14:57:53 +0200

    overlapping has been deleted. Dimension problem has been resolved.

--- bilstm_CL/create_prediction.py
+++ bilstm_CL/create_prediction.py
@@ -5,7 +5,7 @@ from create_numpy_for_audio import create_numpy_for_audio
 
 def create_prediction(filename, 
                     threshold, 
-                    featureplan, overlapping = False):
+                    featureplan):
     
     """"It takes audio file and create prediction via lstm system. If output exceeds
     threshold, we will say there is speaker change.
@@ -32,161 +32,66 @@ def create_prediction(filename,
     
     ix_frame = 0
     
-    if (overlapping):
+    
+    while (ix_frame+799<matrix_of_single_audio.shape[0]):        
+    
+        prediction = model.predict(np.expand_dims(prediction_vector[ix_frame:ix_frame+800], axis=0))
         
-        while ((ix_frame+799)<feature_vector.shape[0]):        
-           
-            prediction = model.predict(np.expand_dims(feature_vector[ix_frame:ix_frame+800], axis=0))
-            prediction = prediction.squeeze(axis=2)
-            prediction = prediction.squeeze(axis=0)
+        prediction_vector.append(prediction)
+        ix_frame += 800
+    
+    prediction_vector = np.asarray(prediction_vector)
+    print (prediction_vector.shape)
 
-            prediction_vector.append(prediction)
-            
-            ix_frame += 200
-            
-        prediction_vector = np.asarray(prediction_vector)
-        prediction_array = np.ravel(prediction_vector)
+    prediction_array = np.ravel(prediction_vector)
 
 
-        prediction_array_sec = []
-        prediction_array_msec = []
-        prediction_array_average = []
-        
-        ix_frame_pred = 0
+    prediction_array_sec = []
+    prediction_array_msec = []
+    ix_frame_pred = 0
 
-        total_prediction = len(prediction_array)
-        
-        print (total_prediction)
+    for pred in prediction_array:
+        if (pred > threshold):
+            ms_version = float(win_len + (ix_frame_pred * hop)) # milisecond version to represent end point of first embed            
+            prediction_array_msec.append(int(ms_version))
+            prediction_array_sec.append(ms_version/1000)
 
-        prediction_array_average[0:200] = prediction_array[0:200]
-        prediction_array_average[200:400] = (prediction_array[200:400]+prediction_array[800:1000]) * 0.5
-        prediction_array_average[400:600] = (prediction_array[400:600]+prediction_array[1000:1200]+
-                                             prediction_array[1600:1800]) * 0.33
-        
-        ix_frame = 600
-        count = 0
-        
-        while ((ix_frame+798)<total_prediction):        
+        ix_frame_pred += 1
 
-            next_frame = ix_frame + (count * 600) 
-            try:
-                prediction_array_average[ix_frame:ix_frame+200] = (prediction_array[next_frame:next_frame+200]+
-                                                                   prediction_array[next_frame+600:next_frame+800]+
-                                                                   prediction_array[next_frame+1200:next_frame+1400]+
-                                                                   prediction_array[next_frame+1800:next_frame+2000]) * 0.25
-            except:
-                pass
-            ix_frame += 200
-            count += 1
+    prediction_array_tenth_ms = np.asarray(prediction_array_msec)/10
 
+    list_cons = [list(group) for group in mit.consecutive_groups(prediction_array_tenth_ms)]
 
-        prediction_array = np.asarray(prediction_array_average)
+    prediction_array_msec_smooth = []
 
-        for pred in prediction_array:
+    for single_list_cons in list_cons:
+        prediction_array_msec_smooth.append(np.mean(single_list_cons)*0.01)
 
-            if (pred > threshold):
-                ms_version = float(win_len + (ix_frame_pred * hop)) # milisecond version to represent end point of first embed            
-                prediction_array_msec.append(int(ms_version))
-                prediction_array_sec.append(ms_version/1000)
+    prediction_array_msec_smooth = np.asarray(prediction_array_msec_smooth)
 
-            ix_frame_pred += 1
+    which_turn = 0
 
+    for single_mean_s in prediction_array_msec_smooth:
+        which_turn += 1
 
-        prediction_array_smooth = []
-        for pred in prediction_array_msec:
-            if (pred-hop not in prediction_array_msec):
-                prediction_array_smooth.append(pred*0.001)
+        try:
+            start_time = float(prediction_array_msec_smooth[which_turn-1])
+            end_time = float(prediction_array_msec_smooth[which_turn])
 
+            if ((start_time+0.5) > end_time):
+                mean_s[which_turn] = ((start_time+end_time) / 2)
+                mean_s = np.delete(mean_s, which_turn-1)
+                which_turn -= 1
+                
+        except:
+            pass
 
-        prediction_array_tenth_ms = np.asarray(prediction_array_msec)/10
+np.savetxt(fname="./prediction_txt/" + filename + "_prediction.txt", 
+            X=prediction_array_msec_smooth, 
+            delimiter=' ', fmt='%1.3f')
 
-        list_cons = [list(group) for group in mit.consecutive_groups(prediction_array_tenth_ms)]
-
-        prediction_array_msec_smooth = []
-
-        for single_list_cons in list_cons:
-            prediction_array_msec_smooth.append(np.mean(prediction_array_msec_smooth)*0.01)
-
-        prediction_array_msec_smooth = np.asarray(prediction_array_msec_smooth)
-
-        which_turn = 0
-
-        for single_mean_s in prediction_array_msec_smooth:
-            which_turn += 1
-
-            try:
-                start_time = float(prediction_array_msec_smooth[which_turn-1])
-                end_time = float(prediction_array_msec_smooth[which_turn])
-
-                if ((start_time+0.5) > end_time):
-                    prediction_array_msec_smooth[which_turn] = ((start_time+end_time) / 2)
-                    prediction_array_msec_smooth = np.delete(mean_s, which_turn-1)
-                    which_turn -= 1
-            except:
-                pass
-        
-    else:
-        while (ix_frame+799<matrix_of_single_audio.shape[0]):        
-        
-            prediction = model.predict(np.expand_dims(prediction_vector[ix_frame:ix_frame+800], axis=0))
-            prediction = prediction.squeeze(axis=2)
-            prediction = prediction.squeeze(axis=0)
-
-            prediction_vector.append(prediction)
-            ix_frame += 800
-        
-        prediction_vector = np.asarray(prediction_vector)
-        print (prediction_vector.shape)
-
-        prediction_array = np.ravel(prediction_vector)
-
-
-        prediction_array_sec = []
-        prediction_array_msec = []
-        ix_frame_pred = 0
-
-        for pred in prediction_array:
-            if (pred > threshold):
-                ms_version = float(win_len + (ix_frame_pred * hop)) # milisecond version to represent end point of first embed            
-                prediction_array_msec.append(int(ms_version))
-                prediction_array_sec.append(ms_version/1000)
-
-            ix_frame_pred += 1
-
-        prediction_array_tenth_ms = np.asarray(prediction_array_msec)/10
-
-        list_cons = [list(group) for group in mit.consecutive_groups(prediction_array_tenth_ms)]
-
-        prediction_array_msec_smooth = []
-
-        for single_list_cons in list_cons:
-            prediction_array_msec_smooth.append(np.mean(single_list_cons)*0.01)
-
-        prediction_array_msec_smooth = np.asarray(prediction_array_msec_smooth)
-
-        which_turn = 0
-
-        for single_mean_s in prediction_array_msec_smooth:
-            which_turn += 1
-
-            try:
-                start_time = float(prediction_array_msec_smooth[which_turn-1])
-                end_time = float(prediction_array_msec_smooth[which_turn])
-
-                if ((start_time+0.5) > end_time):
-                    mean_s[which_turn] = ((start_time+end_time) / 2)
-                    mean_s = np.delete(mean_s, which_turn-1)
-                    which_turn -= 1
-                    
-            except:
-                pass
-
-    np.savetxt(fname="./prediction_txt/" + filename + "_prediction.txt", 
-               X=prediction_array_msec_smooth, 
-               delimiter=' ', fmt='%1.3f')
-
-    return (prediction_array, prediction_array_msec_smooth)
+return (prediction_array, prediction_array_msec_smooth)
 
 if __name__ == "__main__":
-    train_model(sys.argv[1], sys.argv[2],
+    create_prediction(sys.argv[1], sys.argv[2],
                 sys.argv[3], sys.argv[4])
